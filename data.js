72e444258af13a15ee1f676db04284851283fe62
Ramadhani <saputraramadhani216@gmail.com>
2022-12-08 15:08:55 +0700

    Second deployment

--- data.js
+++ data.js
@@ -23,7 +23,146 @@ export const projects = [
     img: "./03-reviews/setup/project-img.png",
   },
   {
-    id: 1,
+    id: 4,
+    name: "Navbar Project",
+    description: "This project teach about how to make responsive navbar",
+    href: "./04-navbar/setup/index.html",
+    img: "./04-navbar/setup/project-img.png",
+  },
+  {
+    id: 5,
+    name: "Sidebar Project",
+    description: "This project teach about how to make responsive sidebar",
+    href: "./05-sidebar/setup/index.html",
+    img: "./05-sidebar/setup/project-img.png",
+  },
+  {
+    id: 6,
+    name: "Modal Project",
+    description: "This project teach about how to display and close modal",
+    href: "./06-modal/setup/index.html",
+    img: "./06-modal/setup/project-img.png",
+  },
+  {
+    id: 7,
+    name: "Question Project",
+    description:
+      "This project teach about how to select node at DOM and how to manipulate it",
+    href: "./07-questions/setup/index.html",
+    img: "./07-questions/setup/project-img.png",
+  },
+  {
+    id: 8,
+    name: "Menu Project",
+    description:
+      "This project teach about how to load the data and dynamically put the data in the DOM",
+    href: "./08-menu/setup/index.html",
+    img: "./08-menu/setup/project-img.png",
+  },
+  {
+    id: 9,
+    name: "Video Project",
+    description:
+      "This project teach about how to put a video background on your web and how to manipulate it",
+    href: "./09-video/setup/index.html",
+    img: "./09-video/setup/project-img.png",
+  },
+  {
+    id: 10,
+    name: "Scroll Project",
+    description:
+      "This project teach about how to made a sticky and responsive navbar ",
+    href: "./10-scroll/setup/index.html",
+    img: "./10-scroll/setup/project-img.png",
+  },
+  {
+    id: 11,
+    name: "Tabs Project",
+    description:
+      "This project teach about how to made a tab menu, selecting node and playing with dataset property",
+    href: "./11-tabs/setup/index.html",
+    img: "./11-tabs/setup/project-img.png",
+  },
+  {
+    id: 12,
+    name: "Countdown Timer Project",
+    description:
+      "This project teach about how to creating and manipulating Date dynamically",
+    href: "./12-countdown-timer/setup/index.html",
+    img: "./12-countdown-timer/setup/project-img.png",
+  },
+  {
+    id: 13,
+    name: "Lorem Ipsum Generator Project",
+    description: "This project teach about how to generate lorem paragraph",
+    href: "./13-lorem-ipsum/setup/index.html",
+    img: "./13-lorem-ipsum/setup/project-img.png",
+  },
+  {
+    id: 14,
+    name: "Grocery List Project",
+    description:
+      "This project teach about how to create a to do list and manipulate it with localStorage",
+    href: "./14-grocery-bud/setup/index.html",
+    img: "./14-grocery-bud/setup/project-img.png",
+  },
+  {
+    id: 15,
+    name: "Slider Project",
+    description:
+      "This project teach about how to manipulating style with javascript",
+    href: "./15-slider/setup/index.html",
+    img: "./15-slider/setup/project-img.png",
+  },
+  {
+    id: 16,
+    name: "Counter Project",
+    description:
+      "This project teach about how to create project with OOP classes",
+    href: "./16-counter/setup/index.html",
+    img: "./16-counter/setup/project-img.png",
+  },
+  {
+    id: 17,
+    name: "Gallery Project",
+    description:
+      "This project teach about how to create project with OOP classes and bind methods",
+    href: "./17-gallery/setup/index.html",
+    img: "./17-gallery/setup/project-img.png",
+  },
+  {
+    id: 18,
+    name: "Numbers Project",
+    description: "This project teach about how to work with setInterval",
+    href: "./18-numbers/setup/index.html",
+    img: "./18-numbers/setup/project-img.png",
+  },
+  {
+    id: 19,
+    name: "Dark Mode Project",
+    description:
+      "This project teach about how work with moment library and selecting documentElement",
+    href: "./19-dark-mode/setup/index.html",
+    img: "./19-dark-mode/setup/project-img.png",
+  },
+  {
+    id: 20,
+    name: "Filters Project",
+    description:
+      "This project teach about how to create project with filter form and using Set, ",
+    href: "./20-filters/setup/index.html",
+    img: "./20-filters/setup/project-img.png",
+  },
+  {
+    id: 21,
+    name: "Dad Jokes Project",
+    description:
+      "This project teach about how to create asynchronous javascript function, try and catch, and fetching rest API",
+    href: "./21-dad-jokes/setup/index.html",
+    img: "./21-dad-jokes/setup/project-img.png",
+  },
+  {
+    id: 22,
     name: "Products Project",
     description:
       "This project teach about how to pas params in URL and how fetching work",
@@ -31,195 +170,49 @@ export const projects = [
     img: "./22-products/setup/project-img.png",
   },
   {
-    id: 1,
-    name: "Products Project",
+    id: 23,
+    name: "Random User Project",
     description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
+      "This project teach about how to fetch data and using export import on Javascript",
+    href: "./23-random-user/setup/index.html",
+    img: "./23-random-user/setup/project-img.png",
   },
   {
-    id: 1,
-    name: "Products Project",
+    id: 24,
+    name: "Cocktails Project",
     description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
+      "This project teach about how to create loading when data is fetching, using export import, and redirecting link using dataset",
+    href: "./24-cocktails/setup/index.html",
+    img: "./24-cocktails/setup/project-img.png",
   },
   {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
+    id: 25,
+    name: "Slider 2 Project",
+    description: "This project teach about how to create smooth slider",
+    href: "./25-slider/setup/index.html",
+    img: "./25-slider/setup/project-img.png",
   },
   {
-    id: 1,
-    name: "Products Project",
+    id: 26,
+    name: "Strip Submenus Project",
     description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
+      "This project teach about how to create submenu with the mouseover event",
+    href: "./26-stripe-submenus/setup/index.html",
+    img: "./26-stripe-submenus/setup/project-img.png",
   },
   {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
+    id: 27,
+    name: "Pagination Project",
+    description: "This project teach about how to create working pagination",
+    href: "./27-pagination/setup/index.html",
+    img: "./27-pagination/setup/project-img.png",
   },
   {
-    id: 1,
-    name: "Products Project",
+    id: 28,
+    name: "Wikipedia Project",
     description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
-  },
-  {
-    id: 1,
-    name: "Products Project",
-    description:
-      "This project teach about how to pas params in URL and how fetching work",
-    href: "./22-products/setup/index.html",
-    img: "./22-products/setup/project-img.png",
+      "This project teach about how to create Wikipedia using wikipedia API",
+    href: "./28-wikipedia/setup/index.html",
+    img: "./28-wikipedia/setup/project-img.png",
   },
 ];
